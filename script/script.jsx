3d8ff752567a667612b9a493d3487a3c7bcc9629
viggoStrom <104303404+viggoStrom@users.noreply.github.com>
2023-01-20 19:52:04 +0100

    setting up jsx

---
+++ script/script.jsx
@@ -0,0 +1,10 @@
+
+const randomIpGen = () => {
+    for (let index = 0; index < 3; index++) {
+        const element = array[index];
+    }    
+}
+
+const ip = 0
+
+const h1 = <h1>Connecting to: {ip}</h1>
