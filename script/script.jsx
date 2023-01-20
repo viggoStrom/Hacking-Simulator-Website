2db474c7d99e81f65249ed13a05efc7ce2c6b263
viggoStrom <104303404+viggoStrom@users.noreply.github.com>
2023-01-20 21:03:09 +0100

    random ip adress done

--- script/script.jsx
+++ script/script.jsx
@@ -1,13 +1,21 @@
 
 const randomIpGen = () => {
+    let randomIPv4 = Math.floor(Math.random() * 256).toString()
+
     for (let index = 0; index < 3; index++) {
-        const element = array[index];
-    }    
+        const number = Math.floor(Math.random() * 256).toString()
+        randomIPv4 = randomIPv4.concat(".",number)
+    }
+    // im dont fully understand this but i know that RE is an appropriate solution to this problem
+    randomIPv4 = randomIPv4.replace(/\d+/g, function (m) {
+        return "00".substring(m.length - 1) + m;
+    });
+
+    return randomIPv4
 }
 
-const ip = 0
 
-const h1 = <h1>Connecting to: {ip}</h1>
+const h1 = <h1>Connecting to: {randomIpGen()}</h1>
 
 const root = ReactDOM.createRoot(document.querySelector("header span"))
 root.render(h1)
