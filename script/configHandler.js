e21a103c0b08044bff30f38cd05dae54c900cdbb
viggoStrom <104303404+viggoStrom@users.noreply.github.com>
2023-02-10 11:44:50 +0100

    continued config with grid

--- script/configHandler.js
+++ script/configHandler.js
@@ -1,5 +1,9 @@
 const configMenu = document.getElementById("configMenu")
 
 function openConfigMenu() {
-    
+    if (configMenu.style.display == "none") {
+        configMenu.style.display = "grid"
+    } else {
+        configMenu.style.display = "none"
+    }
 }
