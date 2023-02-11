bab6e238a1c8d046626b34d5a8b14c29d5ce0649
viggoStrom <104303404+viggoStrom@users.noreply.github.com>
2023-02-11 21:33:37 +0100

    internal storage stuff

--- script/configHandler.js
+++ script/configHandler.js
@@ -1,11 +1,21 @@
 const configMenu = document.getElementById("configMenu")
+const configMenuWrapper = document.getElementById("configMenuAlignmentWrapper")
+
+configMenuWrapper.addEventListener("click", function(event) {
+    event.stopPropagation();
+  });  
 
 function openConfigMenu() {
+    console.log('Clicked:', window.event.target);
+    window.event.stopPropagation()
+
     if (configMenu.style.display == "none") {
         configMenu.style.display = "grid"
+        configMenuWrapper.setAttribute("onClick","openConfigMenu()")
         // text.configOpen = true
     } else {
         configMenu.style.display = "none"
+        configMenuWrapper.setAttribute("onClick","")
         // text.configOpen = false
     }
 }
@@ -19,14 +29,15 @@ function openConfigMenu() {
 //     })
 //     .catch(error => console.error(error));
 
-localStorage.setItem("indentSize", 8)
-localStorage.setItem("indent", "\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0")
-localStorage.setItem("numberOfLinesOnScreen", 27)
-localStorage.setItem("linesPerKeypress", 1)
-localStorage.setItem("textColor", "#008e00")
-localStorage.setItem("backgroundColor", "#111111")
-localStorage.setItem("startingIndex", 0)
-localStorage.setItem("smoothTyping", false)
+if (localStorage.length < 8) {
+    localStorage.setItem("indentSize", 8)
+    localStorage.setItem("indent", "\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0")
+    localStorage.setItem("numberOfLinesOnScreen", "auto") // Math.round(window.innerHeight * .7 / this.heightOfLi) //auto (27 lines)
+    localStorage.setItem("linesPerKeypress", 1)
+    localStorage.setItem("textColor", "#008e00")
+    localStorage.setItem("backgroundColor", "#111111")
+    localStorage.setItem("startingIndex", 0)
+    localStorage.setItem("smoothTyping", false)
+}
 
 console.log(localStorage);
-localStorage.removeItem("config")
--- script/script.jsx
+++ script/script.jsx
@@ -5,7 +5,7 @@ class scrollingText {
         this.initalLi = document.querySelector("section ul li")
         this.heightOfLi = document.querySelector("section ul li").offsetHeight; this.initalLi.remove()
         this.numberOfLinesOnScreen = Math.round(window.innerHeight * .7 / this.heightOfLi) //auto (27 lines)
-        this.linesPerKeypress = 2
+        this.linesPerKeypress = 1
         this.textColor = "#008e00"
         this.backgroundColor = "#111111"
         this.startingIndex = 0
@@ -37,23 +37,6 @@ class scrollingText {
         }
     }
 
-    async fetchConfig() {
-        const requestURL = "./script/json/config.json"
-        const request = new Request(requestURL)
-        console.log("config file: request sent");
-
-        console.log("config file: awaiting response");
-        const response = await fetch(request)
-        const json = await response.json()
-        console.log("config file: response recieved");
-        try {
-            this.config = json
-        } catch (error) {
-            console.error("config file: failed to fetch config (json)");
-            this.fetchConfig()
-        }
-    }
-
     updateConfig() {
         this.indent = ""
         for (let index = 0; index < this.indetSize; index++) {
@@ -69,7 +52,7 @@ class scrollingText {
     }
 
     writeRow() {
-        let codeSnippets = this.text.rows // to make sure the json files contents is used
+        const codeSnippets = this.text.rows // to make sure the json files contents is used
 
         if (this.index >= this.text.rows.length - 1) {
             this.index = this.startingIndex - 1
@@ -103,21 +86,21 @@ class scrollingText {
             "function addIconToPanel(statusIcon) {",
             "\tif (!(statusIcon instanceof BaseStatusIcon))",
             "\t\tthrow TypeError(`Unexpected icon type: ${statusIcon}`);",
-            "",
+            "\t",
             "\tconst settings = SettingsManager.getDefaultGSettings();",
             "\tconst indicatorId = `appindicator-${statusIcon.uniqueId}`;",
-            "",
+            "\t",
             "\tconst currentIcon = Main.panel.statusArea[indicatorId];",
             "\tif (currentIcon) {",
             "\t\tif (currentIcon !== statusIcon)",
             "\t\t\tcurrentIcon.destroy();",
-            "",
+            "\t",
             "\t\tMain.panel.statusArea[indicatorId] = null;",
             "\t}",
-            "",
+            "\t",
             "\tMain.panel.addToStatusArea(indicatorId, statusIcon, 1,",
             "\t\tsettings.get_string('tray-pos'));",
-            "",
+            "\t",
             "\tUtil.connectSmart(settings, 'changed::tray-pos', statusIcon, () =>",
             "\t\taddIconToPanel(statusIcon));",
             "}",
